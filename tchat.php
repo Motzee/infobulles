fb2d291b099b1611f400f854b63d08077bf9e7d3
Motzee <maelle.perron.26@gmail.com>
2017-11-10 06:48:09 +0100

    Avancée ossature

---
+++ tchat.php
@@ -0,0 +1,19 @@
+<?php session_start() ; ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	
+<?php require_once("templates/architect_head.html") ; ?>
+
+<body>
+
+<?php
+    include_once("templates/architect_header.html") ;
+	
+    include_once("templates/v_tchat.html") ;
+	
+    include_once("templates/architect_footer.html") ;
+?>
+
+</body>
+</html>
